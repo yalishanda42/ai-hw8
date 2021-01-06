b9843405bed63abda225fd9d6a61d00e8bcfd939
Alexander Ignatov <yalishanda@abv.bg>
2021-01-06 12:26:39 +0200

    Init

--- Sources/NeuralNetwork/NeuralNetwork.swift
+++ Sources/NeuralNetwork/NeuralNetwork.swift
@@ -1,3 +1,81 @@
-struct NeuralNetwork {
-    var text = "Hello, World!"
+import RealModule  // for Double.exp(_:)
+
+func sigmoid(_ x: Double) -> Double {
+    return 1 / (1 + Double.exp(-x))
+}
+
+protocol Node: AnyObject {
+    func activate() -> Double
+}
+
+class InputNode: Node {
+    var value: Double
+    
+    init(value: Double = 0.0) {
+        self.value = value
+    }
+    
+    func activate() -> Double { value }
+}
+
+class Connection {
+    let node: Node
+    var weight: Double
+    
+    init(node: Node, weight: Double) {
+        self.node = node
+        self.weight = weight
+    }
+}
+
+class NeuronNode: Node {
+    let inputs: [Connection]
+    
+    init(inputs: [Connection]) {
+        self.inputs = inputs
+    }
+    func activate() -> Double {
+        let sum = inputs
+            .map { $0.node.activate() * $0.weight }
+            .reduce(0, +)
+        return sigmoid(sum)
+    }
+}
+
+/// Neural network containing 1 hidden layer that uses backpropagation.
+public class NeuralNet {
+    var inputLayer: [InputNode]
+    let hiddenLayer: [NeuronNode]
+    let outputLayer: [NeuronNode]
+    
+    /// Return a double in the range `[-0.05; +0.05]`
+    private static var randomInitialWeight: Double {
+        .random(in: -0.05...0.05)
+    }
+    
+    public init(inputNodes: Int, hiddenNodes: Int, outputNodes: Int) {
+        let inputLayerNodes = Array(repeating: InputNode(), count: inputNodes)
+        self.inputLayer = inputLayerNodes
+        let hiddenLayerNodes =  Array(0..<hiddenNodes).map { _ in
+            NeuronNode(inputs: inputLayerNodes.map { inpnode in
+                Connection(
+                    node: inpnode,
+                    weight: NeuralNet.randomInitialWeight
+                )
+            })
+        }
+        self.hiddenLayer = hiddenLayerNodes
+        let outputLayerInputNodes: [Node] = hiddenLayerNodes.isEmpty
+            ? inputLayerNodes
+            : hiddenLayerNodes
+        let outputLayerNodes = Array(0..<outputNodes).map { _ in
+            NeuronNode(inputs: outputLayerInputNodes.map { inpnode in
+                Connection(
+                    node: inpnode,
+                    weight: NeuralNet.randomInitialWeight
+                )
+            })
+        }
+        self.outputLayer = outputLayerNodes
+    }
 }
--- Tests/NeuralNetworkTests/NeuralNetworkTests.swift
+++ Tests/NeuralNetworkTests/NeuralNetworkTests.swift
@@ -2,14 +2,25 @@ import XCTest
 @testable import NeuralNetwork
 
 final class NeuralNetworkTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(NeuralNetwork().text, "Hello, World!")
+    func testNNInit0HiddenNodes() {
+        let nn = NeuralNet(inputNodes: 2, hiddenNodes: 0, outputNodes: 1)
+        XCTAssertEqual(nn.inputLayer.count, 2)
+        XCTAssertEqual(nn.hiddenLayer.count, 0)
+        XCTAssertEqual(nn.outputLayer.count, 1)
+        XCTAssertEqual(nn.outputLayer[0].inputs.count, 2)
+    }
+    
+    func testNNInit4HiddenNodes () {
+        let nn = NeuralNet(inputNodes: 2, hiddenNodes: 4, outputNodes: 1)
+        XCTAssertEqual(nn.inputLayer.count, 2)
+        XCTAssertEqual(nn.hiddenLayer.count, 4)
+        XCTAssertEqual(nn.hiddenLayer[0].inputs.count, 2)
+        XCTAssertEqual(nn.outputLayer.count, 1)
+        XCTAssertEqual(nn.outputLayer[0].inputs.count, 4)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testNNInit0HiddenNodes", testNNInit0HiddenNodes),
+        ("testNNInit4HiddenNodes", testNNInit4HiddenNodes),
     ]
 }
