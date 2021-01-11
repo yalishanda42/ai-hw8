f6b5b035f9fc84ab318da72775ca644fd3b1a1c6
Alexander Ignatov <yalishanda@abv.bg>
2021-01-11 18:42:44 +0200

    Refactor defaults

--- Sources/NeuralNetwork/NeuralNetwork.swift
+++ Sources/NeuralNetwork/NeuralNetwork.swift
@@ -58,23 +58,32 @@ public class NeuralNet {
     let hiddenLayer: [NeuronNode]
     let outputLayer: [NeuronNode]
     
+    let weightUpdateFactor: Double
+    
     /// Return a double in the range `[-0.05; +0.05]`
-    private static var randomInitialWeight: Double {
+    public class func deafultRandomInitialWeightFunction() -> Double {
         .random(in: -0.05...0.05)
     }
     
-    private static let weightUpdateFactor = 0.1
+    public static let deafultWeightUpdateFactor = 0.1
     
-    public init(inputNodes: Int, hiddenNodes: Int, outputNodes: Int, bias: Double? = nil) {
+    public init(
+        inputNodes: Int,
+        hiddenNodes: Int,
+        outputNodes: Int,
+        bias: Double? = nil,
+        initialWeightFunction: () -> Double = NeuralNet.deafultRandomInitialWeightFunction,
+        weightUpdateFactor: Double = NeuralNet.deafultWeightUpdateFactor
+    ) {
+        self.weightUpdateFactor = weightUpdateFactor
         let inputLayerNodes = Array(repeating: InputNode(), count: inputNodes)
         self.inputLayer = inputLayerNodes
-        let hiddenLayerNodes = NeuralNet.initializeNeuronNodeLayer(nodesCount: hiddenNodes, inputLayer: inputLayerNodes, bias: bias)
+        let hiddenLayerNodes = NeuralNet.initializeNeuronNodeLayer(nodesCount: hiddenNodes, inputLayer: inputLayerNodes, bias: bias, initialWeightFunction: initialWeightFunction)
         self.hiddenLayer = hiddenLayerNodes
         let outputLayerInputNodes: [Node] = hiddenLayerNodes.isEmpty
             ? inputLayerNodes
             : hiddenLayerNodes
-        let outputLayerNodes = NeuralNet.initializeNeuronNodeLayer(nodesCount: outputNodes, inputLayer: outputLayerInputNodes, bias: bias)
-        
+        let outputLayerNodes = NeuralNet.initializeNeuronNodeLayer(nodesCount: outputNodes, inputLayer: outputLayerInputNodes, bias: bias, initialWeightFunction: initialWeightFunction)
         self.outputLayer = outputLayerNodes
     }
     
@@ -92,7 +101,7 @@ public class NeuralNet {
                 let actualOutputs = predict(exampleInput)
                 let hiddenNodesOutputs = hiddenLayer.map { $0.activate() }
                 
-                // Backpropagate error
+                // Calculate error
                 let outputNodesError = zip(actualOutputs, exampleOutput).map {
                     $0 * (1 - $0) * (1 + $1)
                 }
@@ -105,19 +114,9 @@ public class NeuralNet {
                         }.reduce(0, +)
                     }
                 
-                for (i, outputNode) in outputLayer.enumerated() {
-                    for connection in outputNode.inputs {
-                        let update = NeuralNet.weightUpdateFactor * outputNodesError[i] * connection.node.activate()
-                        connection.weight += update
-                    }
-                }
-                
-                for (i, hiddenNode) in hiddenLayer.enumerated() {
-                    for connection in hiddenNode.inputs {
-                        let update = NeuralNet.weightUpdateFactor * hiddenNodesError[i] * connection.node.activate()
-                        connection.weight += update
-                    }
-                }
+                // Backpropagate error
+                updateLayerNodes(outputLayer, calculatedError: outputNodesError)
+                updateLayerNodes(hiddenLayer, calculatedError: hiddenNodesError)
             }
         }
     }
@@ -135,7 +134,12 @@ public class NeuralNet {
 // MARK: - Helpers
 
 private extension NeuralNet {
-    class func initializeNeuronNodeLayer(nodesCount: Int, inputLayer: [Node], bias: Double?) -> [NeuronNode] {
+    class func initializeNeuronNodeLayer(
+        nodesCount: Int,
+        inputLayer: [Node],
+        bias: Double?,
+        initialWeightFunction: () -> Double
+    ) -> [NeuronNode] {
         Array(0..<nodesCount).map { _ in
             NeuronNode(
                 inputs: [Node](
@@ -145,10 +149,20 @@ private extension NeuralNet {
                     ).map { inputNode in
                         Connection(
                             node: inputNode,
-                            weight: NeuralNet.randomInitialWeight
+                            weight: initialWeightFunction()
                         )
                     }
             )
+        }
+    }
+    
+    func updateLayerNodes(_ nodes: [NeuronNode], calculatedError: [Double]) {
+        assert(nodes.count == calculatedError.count, "Not all nodes have errors calculated!")
+        for (i, outputNode) in nodes.enumerated() {
+            for connection in outputNode.inputs {
+                let update = weightUpdateFactor * calculatedError[i] * connection.node.activate()
+                connection.weight += update
+            }
         }
     }
 }
